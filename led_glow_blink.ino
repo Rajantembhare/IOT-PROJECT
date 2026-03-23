65d046d98e5560220fe8b6da01af7c393c8fa2ef
Rajantembhare <tembharerajan459@gmail.com>
2026-03-23 23:43:54 +0530

    Commit5

---
+++ led_glow_blink.ino
@@ -0,0 +1,19 @@
+//this is my first iot program
+
+#define my_led 7 // global declaration, LED pin no.7
+
+void setup() {
+
+pinMode (my led, OUTPUT);
+
+void loop() {
+
+digitalWrite(my_led, HIGH);
+
+delay(1000);
+
+digitalWrite(my_led, LOW);
+
+delay(1000);
+
+}
